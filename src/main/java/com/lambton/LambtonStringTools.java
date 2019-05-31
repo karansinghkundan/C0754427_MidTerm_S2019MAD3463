a2170046af3f75d2c6ea207ca470a4131f1539cf
MacStudent <macstudent@130-IMAC-40.local>
2019-05-31 19:00:50 -0400

    Initial commit

---
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -0,0 +1,22 @@
+package com.lambton;
+
+import java.util.Scanner;
+
+public class LambtonStringTools {
+    public static void main(String[] args) {
+        String x, reverse =" ";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a String to be reversed");
+        x = in.nextLine();
+        int length = x.length();
+        for (int i =length-1;i >=0;i--)
+            r
+
+
+
+    }
+
+
+
+    }
+
